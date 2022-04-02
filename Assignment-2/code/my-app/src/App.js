97934dee9634da6c6df618c0368629ddef644c55
Arvaaz <arvazsaifi17@gmail.com>
2022-04-02 15:49:18 +0530

    Assignment-2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -5,24 +5,26 @@ function App(props) {
   const { employee } = props;
   const { name, age, location, bloodGroup, profileImg } = props.employee;
   return (
-    <div className="container">
-      {/* Start your React code here */}
-      <div>
-        <img clasName="img" src={profileImg} alt="image"></img>
+    (
+      <div className="container">
+        {/* Start your React code here */}
+        <div className="image">
+          <img src={profileImg} alt="profile-pic" ></img>
+        </div>
+        <div className='data'>
+          <h2>{name}</h2>
+          <br></br>
+          <p>location</p>
+          <h3>{location}</h3>
+          <br></br>
+          <p>bloodGroup</p>
+          <h3>{bloodGroup}</h3>
+          <br></br>
+          <p>Age</p>
+          <h3>{age}</h3>
+        </div>
       </div>
-      <div className='data'>
-        <h1>{name}</h1>
-        <br></br>
-        <p>Location</p>
-        <h3>{location}</h3>
-        <br></br>
-        <p>Blood Group</p>
-        <h3>{bloodGroup}</h3>
-        <br></br>
-        <p>Age</p>
-        <h3>{age}</h3>
-      </div>
-    </div>
+    )
   )
 }
 
