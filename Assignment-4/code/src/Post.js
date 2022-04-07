052f163cc23358551dfe45f147fab4c43efa657e
Arvaaz <arvazsaifi17@gmail.com>
2022-04-07 16:19:21 +0530

    Assignment-4 submission

---
+++ Assignment-4/code/src/Post.js
@@ -0,0 +1,17 @@
+import PostHeader from "./PostHeader";
+import PostImage from "./PostImage";
+import PostFooter from "./PostFooter";
+// import Header from "./Header";
+
+// layout
+const Post = ({ config }) =>{
+    return (
+    <div className="PostContainer">
+        {/* <Header config={config}/> */}
+        <PostHeader config={config}/>
+        <PostImage config={config}/>
+        <PostFooter config={config}/>
+    </div>
+    );
+};
+export default Post;
